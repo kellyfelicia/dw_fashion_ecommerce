48540278ab905ea121b1dfc4cd6a5371113e5fb5
Kelly <142735071+kellyfelicia@users.noreply.github.com>
2024-10-15 19:23:22 +0700

    Add files via upload

---
+++ Schedule Job/schedule_etl_dw.bat
@@ -0,0 +1,1 @@
+C:\Program Files\data-integration\kitchen.bat /file:"C:\Users\Kelly\OneDrive\Data Warehouse\UAS\Transformation\job_dw_ecommerce.kjb" /level:Basic > "C:\Users\Kelly\OneDrive\Data Warehouse\UAS\log_dw.txt"
